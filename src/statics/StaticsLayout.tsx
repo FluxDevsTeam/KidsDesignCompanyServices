7ada54f2f4b88b867442c8c1dc58dbd3b762fcaf
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-12-30 21:49:59 +0100

    style(statics): remove duplicate import in StaticsLayout

--- src/statics/StaticsLayout.tsx
+++ src/statics/StaticsLayout.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
